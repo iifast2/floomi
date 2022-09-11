4a767762d71ee046c65664007320cf22e679b66e
iifast2 <60607576+iifast2@users.noreply.github.com>
2022-09-12 00:20:21 +0100

    commit 6 - Profilescreens - Edit Profile
    :white_check_mark:  commit 6 - Profilescreens - Edit Profile
    
    profilescreen.dart + Changing some Colors in profile buttons

--- lib/screens/profilescreen.dart
+++ lib/screens/profilescreen.dart
@@ -13,9 +13,9 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-
   String username = '';
   bool dataisthere = false;
+  TextEditingController usernamecontroller = TextEditingController();
 
   @override
   void initState() {
@@ -24,89 +24,151 @@ class _ProfileScreenState extends State<ProfileScreen> {
     getuserdata();
   }
 
-  getuserdata()async{
-  DocumentSnapshot userdoc = await usercollection.doc(FirebaseAuth.instance.currentUser?.uid).get();
-  setState(() {
-    username = userdoc?['username'] ?? '';          // Solved: https://stackoverflow.com/a/72699569
-    dataisthere = true;
-  });
+  getuserdata() async {
+    DocumentSnapshot userdoc =
+        await usercollection.doc(FirebaseAuth.instance.currentUser?.uid).get();
+    setState(() {
+      username = userdoc['username'] ??
+          ''; // Solved: https://stackoverflow.com/a/72699569
+      dataisthere = true;
+    });
+  }
+
+  editprofile() async {
+    usercollection
+        .doc(FirebaseAuth.instance.currentUser?.uid)
+        .update({'username': usernamecontroller.text});
+
+    setState(() {
+      username = usernamecontroller.text;
+    });
+    Navigator.pop(context);
+  }
+
+  openeditprofiledialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return Dialog(
+              child: Container(
+            height: 200,
+            child: Column(
+              children: [
+                // I added a Const
+                SizedBox(
+                  height: 30,
+                ),
+                //  TextField(style: mystyle(18),Colors.black),    // I added cursorColor
+                Container(
+                  margin: EdgeInsets.only(left: 30, right: 30),
+                  child: TextField(
+                    controller: usernamecontroller,
+                    style: mystyle(18),
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      labelText: "Update Username",
+                      labelStyle: mystyle(16, Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                InkWell(
+                  onTap: () => editprofile(),
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 40,
+                    decoration: const BoxDecoration(
+                        gradient:
+                            LinearGradient(colors: GradientColors.juicyOrange)),
+                    // color: marbleWall
+                    child: Center(
+                        child: Text("Update Now!",
+                            style: mystyle(
+                                17)) // , Colors.white // I couldn't add them
+
+                        ),
+                  ),
+                )
+              ],
+            ),
+          ));
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     //var face;
     return Scaffold(
-
-        backgroundColor: Colors.grey[250],
-        body: dataisthere == false ? const Center(
-          child: CircularProgressIndicator(),
-        ):  Stack(
-          children: [
-            ClipPath(
-              clipper: OvalBottomBorderClipper(),  //import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
-              child: Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height /2.5,
-                decoration: const BoxDecoration(                        // I added Const here
-                  gradient: LinearGradient(colors: GradientColors.facebookMessenger),
-                ),
-
-              ),
-            ),
-
-            Container(
-             margin: EdgeInsets.only(
-             left: MediaQuery.of(context).size.width /2 - 64,
-             top: MediaQuery.of(context).size.height /3.1,
-
-              ),
-
-             child: const CircleAvatar(                                           // I added Const here
-               radius: 64,
-               backgroundImage: NetworkImage('https://cdn-icons-png.flaticon.com/512/1177/1177568.png'),  // credit : Freepik.com
-              ),
-            ),
-            Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 300,),
-                  Text(username,style: mystyle(40, Colors.black),
-                  textAlign: TextAlign.center,
-                  ),
-                const SizedBox(height: 30,),
-                Container(
-                 width: MediaQuery.of(context).size.width /2,
-                 height: 40,
-                 decoration: BoxDecoration(
-                   gradient: LinearGradient(colors: GradientColors.cherry)
-                 ),
-                child: Center(
-                  child: Text("Edit Profile", style: mystyle(17))  // , Colors.white // I couldn't add them
-                  
-                ),
-
-                )
-                ],
-              ),
+      backgroundColor: Colors.grey[250],
+      body: dataisthere == false
+          ? const Center(
+              child: CircularProgressIndicator(),
             )
+          : Stack(
+              children: [
+                ClipPath(
+                  clipper: OvalBottomBorderClipper(),
+                  //import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
+                  child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height / 2.5,
+                    decoration: const BoxDecoration(
+                      // I added Const here
+                      gradient: LinearGradient(
+                          colors: GradientColors.facebookMessenger),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    left: MediaQuery.of(context).size.width / 2 - 64,
+                    top: MediaQuery.of(context).size.height / 3.1,
+                  ),
+                  child: const CircleAvatar(
+                    // I added Const here
+                    radius: 64,
+                    backgroundImage: NetworkImage(
+                        'https://cdn-icons-png.flaticon.com/512/1177/1177568.png'), // credit : Freepik.com
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(
+                        height: 300,
+                      ),
+                      Text(
+                        username,
+                        style: mystyle(40, Colors.black),
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      InkWell(
+                        onTap: () => openeditprofiledialog(),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width / 2,
+                          height: 40,
+                          decoration: const BoxDecoration(
+                              gradient: LinearGradient(
+                                  colors: GradientColors.meanFruit)),
+                          child: Center(
+                              child: Text("Edit Profile",
+                                  style: mystyle(
+                                      17)) // , Colors.white // I couldn't add them
 
-          ],
-        ),
-
+                              ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
